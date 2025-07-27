74a1e108656ace18081243ec5eb317f951e630c8
mauriciotorocea <mauriciotorocea@gmail.com>
2025-07-27 02:11:13 -0400

    ctepiv0.1.1 update

---
+++ R/Piy.R
@@ -0,0 +1,152 @@
+#' Calculate the Causal Treatment Effect (CTE) distribution for real data with dichotomous outcome
+#'
+#' This function calculates the Causal Treatment Effect (CTE) distribution for real data with a dichotomous outcome variable.
+#'
+#' @param Yobs A vector of observed outcomes. A dichotomous variable. If \code{Yobs} is not dichotomous, it is 
+#' transformed into a dichotomous variable using the threshold \code{y}.
+#' @param Zobs A vector of observed treatments (\code{0} or \code{1}).
+#' @param X A matrix of covariates for each individual. It can be used for calculating conditional \eqn{CTE(0|X=x)} distributions.
+#' @param p A vector of prior propensity values for which the CTE distribution is calculated. If \code{p} is equal to "PY1K1", then the prior propensity \eqn{p} is set equal to the probability \eqn{P(Y=1|K=1)}.
+#' @param y If \code{NULL}, \code{Yobs} is treated as a dichotomous variable. If provided, it is used as a threshold to create 
+#' a dichotomous outcome variable: \code{Y > y}.
+#' @param covariates A logical value indicating whether the \eqn{CTE(0)} distribution should be calculated for each subset defined by the covariates 
+#' (\code{TRUE}) or for the whole dataset (\code{FALSE}).
+#' @param alpha Significance of the credible interval of Pi.
+#' @param suppressMessages If suppressMessages is TRUE, messages of dichomization of Y are ommited.
+#' @param approxtonorm If approxtonorm is TRUE, normal approximation of the distribution of Pi is used.
+#'
+#' @details 
+#' \code{Piy} accepts observed data \code{(Yobs, Zobs, X)}. Each record corresponds to an observation for an individual. 
+#' If \eqn{\mathcal{M}} represents the set of labels for the individuals, then \code{(Yobs, Zobs, X)} are variables in \eqn{\mathcal{M}}.
+#' The outcome \eqn{Y} is defined over the space \eqn{\mathcal{M} \times \{0, 1\}}. \eqn{Y} has at least \code{M} missing data points (counterfactuals).
+#' Each missing outcome can take values \code{0} or \code{1}.
+#' 
+#' Consider the space of all possible worlds, where for each world there exists an outcome \eqn{Y^*}, where \eqn{Y^*} coincides 
+#' with \code{Yobs} for the observed statistical units. For each \eqn{Y^*}, the \eqn{CTE(0)} is calculated. \eqn{CTE(0)} is a random 
+#' variable over all possible worlds.
+#'
+#' The value \eqn{p} represents the prior probability that each unobserved outcome is \code{1}. This prior probability is 
+#' called the "propensity for success." Assuming a common \eqn{p} for all unobserved outcomes implies the absence of a causal effect.
+#' Based on \eqn{p}, the probability of each possible world is determined. If all possible worlds are equally likely, then \eqn{p = 0.5}, 
+#' implying no causal effect. This assumption is unrealistic when the outcome has a probability of success significantly 
+#' different from \code{0.5}.
+#' 
+#' The \eqn{CTE(0)} distribution over the space of possible worlds is calculated for different values of the prior propensity \eqn{p}. 
+#' This allows for the assessment of the impact of the assumption about \eqn{p} on the CTE(0) distribution, and consequently, 
+#' the potential existence of a positive or negative causal effect.
+#' 
+#' The function \code{plotCTEprob} can be used to visualize the probabilities of positive, negative, or no causal effect 
+#' for a given prior propensity \eqn{p}.
+#'
+#' @return 
+#' If \code{covariates} is \code{FALSE}, \code{Piy} returns a list with the CTE(0) distribution over the possible worlds
+#' for each value in the \code{p} vector. The list also includes \code{probMZ}, containing probabilities identified in the 
+#' \eqn{\mathcal{M} \times \{0, 1\}} space:
+#' \itemize{
+#'   \item{\code{P(K=1|Z=1)}}: Probability that \eqn{K=1} given \eqn{Z=1}.
+#'   \item{\code{P(K=1|Z=0)}}: Probability that \eqn{K=1} given \eqn{Z=0}.
+#'   \item{\code{P(Y=1|Z=1,K=1)}}: Probability that \eqn{Y=1} given \eqn{Z=1} and \eqn{K=1}.
+#'   \item{\code{P(Y=1|Z=0,K=1)}}: Probability that \eqn{Y=1} given \eqn{Z=0} and \eqn{K=1}.
+#'   \item{\code{p0}}: The prior propensity where the probability of a positive causal effect is equal to the probability of a negative causal effect.
+#' }
+#' 
+#' If \code{covariates} is \code{TRUE}, \code{Piy} returns a list for each subset defined by the covariates \code{X}. 
+#' The name of each list corresponds to the values of the covariates. If the covariate is a factor, the name of each list element 
+#' refers to the level number rather than the label of the level. For each partition, \code{probMZ} also includes the number of labels 
+#' for the partition (\code{M}) and the number of missing data (\code{NA}) in the outcome.
+#'
+#'
+#' @export
+Piy <- function( Yobs, Zobs, X=NULL, p = c(0:60)/60 , y=NULL , covariates = F, alpha=0.05, suppressMessages=FALSE, approxtonorm=FALSE) {
+  if ( length(Yobs) != length(Zobs) ) stop("Yobs and Zobs must have the same length")
+  
+  if ( !is.null(X) ) {
+    if ( is.null(dim(X)) ) stop("X must be a matrix (or a dataframe).")
+    if ( nrow(X) != length(Yobs) ) stop("X must have the same number of rows that Yobs.")
+    Xunique <- X |> unique()
+  } else {
+    if( covariates ) stop("X is NULL and covariates is TRUE.")
+  }
+  if ( any(is.character(p)) & ( length(p) > 1 | ! any(p == "PY1K1") ) ) {
+    stop("Insert a valid value for p.")
+  }
+  
+  if ( is.null(y) ) {
+  } else {
+    if( !suppressMessages ) { message(paste0("The outcome variable is 1 if Yobs > y, 0 if Yobs ≤ y, and NA if Yobs is NA. y=",y,".")) }
+    Yobs <- 1*(Yobs > y)
+  }
+  
+  nNAZ1 <- sum(is.na(Yobs[Zobs==1]))
+  nNAZ0 <- sum(is.na(Yobs[Zobs==0]))
+  
+  if ( !covariates ) {
+    
+    if ( length(p) > 1 ) {
+      pp <- p
+    } else {
+      if ( p == "PY1K1" ) {
+        pp <- mean(Yobs==1, na.rm = T)
+      } else {
+        pp <- p
+      }
+    }
+    
+    probs <- possibleworlds( M = length(Yobs) , nNAZ1 = nNAZ1, nNAZ0 = nNAZ0, 
+                             nZ1K1 = sum(Zobs[ !is.na(Yobs) ]),
+                             nZ0K1 = sum(Zobs[ !is.na(Yobs) ] == 0),
+                             nY1Z1K1 = sum(Yobs==1 & Zobs == 1 , na.rm = T), 
+                             nY1Z0K1 = sum(Yobs==1 & Zobs == 0 , na.rm = T), 
+                             p = pp , alpha=alpha , approxtonorm=approxtonorm )
+    probs$data <- list( Yobs=Yobs, Zobs=Zobs, X=X, y=y)
+    # gamma factor: proportion between CTEign and E(Pi)
+    if ( length(p) == 1 & any(p == "PY1K1") ) {
+      M <- length(Yobs)
+      nZ1K1 <- sum(Zobs[ !is.na(Yobs) ])
+      nZ0K1 <- sum(Zobs[ !is.na(Yobs) ] == 0)
+      probK1Z1 <- nZ1K1 / M   # (M - nNAZ1) / M  <-- malo
+      probK1Z0 <- nZ0K1 / M   # (M - nNAZ0) / M  <-- malo
+      probs$gamma <- 0.5 * ( 1/probK1Z1 + 1/probK1Z0 )
+    }
+  } else {
+    
+    probs <- list()
+    for ( i in 1:nrow(Xunique) ) {
+      filtro <- apply(X, 1, function(row) all(row == Xunique[i, ])) 
+      Yaux <- Yobs[filtro]
+      Zaux <- Zobs[filtro]
+      Xaux <- X[filtro,]
+      
+      if ( length(p) == 1 & any(p == "PY1K1") ) {
+        pp <- mean(Yaux==1, na.rm = T)
+      } else {
+        pp <- p
+      }
+      
+      probs[[i]] <- possibleworlds( M = sum(filtro) , nNAZ1 = sum(is.na(Yaux[Zaux==1])),
+                                    nNAZ0 = sum(is.na(Yaux[Zaux==0])),
+                                    nZ1K1 = sum(Zaux[ !is.na(Yaux) ]),
+                                    nZ0K1 = sum(Zaux[ !is.na(Yaux) ] == 0),
+                                    nY1Z1K1 = sum(Yaux==1 & Zaux == 1 , na.rm = T), 
+                                    nY1Z0K1 = sum(Yaux==1 & Zaux == 0 , na.rm = T), 
+                                    p = pp  , alpha=alpha, approxtonorm=approxtonorm )
+      names(probs)[i] <- paste0(paste0(colnames(X),".", Xunique[i, ]), collapse = ",")
+      probs[[i]]$probMZ$M   <- sum(filtro)
+      probs[[i]]$probMZ$nNAZ1 <- sum(is.na(Yaux[Zaux==1]))
+      probs[[i]]$probMZ$nNAZ0 <- sum(is.na(Yaux[Zaux==0]))
+      probs[[i]]$data <- list( Yobs=Yaux, Zobs=Zaux, X=Xaux, y=y)
+      # gamma factor: proportion between CTEign and E(Pi)
+      if ( length(p) == 1 & any(p == "PY1K1") ) {
+        M <- sum(filtro)
+        nZ1K1 <- sum(Zaux[ !is.na(Yaux) ])
+        nZ0K1 <- sum(Zaux[ !is.na(Yaux) ] == 0)
+        probK1Z1 <- nZ1K1 / M   # (M - nNAZ1) / M  <-- malo
+        probK1Z0 <- nZ0K1 / M   # (M - nNAZ0) / M  <-- malo
+        probs[[i]]$gamma <- 0.5 * ( 1/probK1Z1 + 1/probK1Z0 )
+      }
+    }
+  }
+  
+  probs
+}
+
---
+++ R/possibleworlds.R
@@ -0,0 +1,218 @@
+#' Calculate the distribution of the Causal Treatment Effect (CTE) for a Dichotomous Outcome
+#'
+#' This function calculates the CTE(0) distribution for a dichotomous outcome over all possible worlds
+#' that are consistent with the observed data. 
+#'
+#' @param M Total number of individuals in the population.
+#' @param nNAZ1,nNAZ0 Use only if nNA is NULL. Number of missing data in the observed outcome for Zobs=1 and Zobs=0.
+#' @param nNA Number of missing data in the observed outcome. If nNA is NULL, nNAZ1+nNAZ0 is used.
+#' @param nZ1K1 Number of statistical units with Z=1 and K=1.
+#' @param nZ0K1 Number of statistical units with Z=0 and K=1.
+#' @param nY1Z1K1 Number of statistical units with Y=1, Z=1, and K=1.
+#' @param nY1Z0K1 Number of statistical units with Y=1, Z=0, and K=1.
+#' @param p Propensity values for which the CTE distribution is calculated.
+#' @param dec.prec Number of decimal places for precision in the calculations (default is 13).
+#' @param alpha Significance of the credible interval of Pi.
+#' @param approxtonorm If approxtonorm is TRUE, normal approximation of the distribution of Pi is used.
+#'
+#' @details 
+#' The variable \eqn{\mathcal{M}} represents the set of labels for the individuals in the population of interest.
+#' Observed causal inference data are typically presented as variables defined for each individual \eqn{m \in M}.
+#' The outcome \eqn{Y} is defined over the space \eqn{\mathcal{M} \times \{0, 1\}}, where \eqn{K(m, z) = 1} if \eqn{Y(m, z)} is observed, 
+#' and 0 otherwise. \eqn{Z} is the projection of \eqn{(m, z)} in \eqn{Z}, meaning \eqn{Z(m, z) = z}.
+#'
+#' The outcome \eqn{Y} is dichotomous, and each missing outcome value can take either 0 or 1. 
+#' Consider the space of all possible worlds where for each world, there exists an outcome \eqn{Y^*},
+#' where \eqn{Y^*} coincides with the observed outcome \eqn{Y_{\text{obs}}} for the observed statistical units.
+#' For each \eqn{Y^*}, the CTE(0) is computed. The CTE(0) is a random variable over all possible worlds.
+#' 
+#' The value \eqn{p} represents the prior probability that each unobserved outcome is 1. This prior probability is denoted as
+#' the propensity for success. Assuming a common \eqn{p} for all unobserved outcomes implies the absence of a causal effect.
+#' Based on \eqn{p}, the probability of each possible world is determined. If all possible worlds are equally likely, \eqn{p = 0.5},
+#' which assumes no causal effect. This assumption becomes unrealistic when the outcome's success probability is significantly 
+#' different from 0.5.
+#'
+#' @return 
+#' A list containing the CTE(0) distribution for a dichotomous outcome in the measurable space. The list includes:
+#' 
+#' \item{masses}{A data frame with the probability mass \eqn{P(CTE(0) = cte)}.}
+#' \item{cdf}{A data frame with the cumulative probability \eqn{P(CTE(0) \leq cte)}.}
+#' \item{probMZ}{Probabilities identified from the data:}
+#'  \itemize{
+#'    \item{probK1Z1}{Probability \eqn{P(K = 1 | Z = 1)}.}
+#'    \item{probK1Z0}{Probability \eqn{P(K = 1 | Z = 0)}.}
+#'    \item{probY1Z1K1}{Probability \eqn{P(Y = 1 | Z = 1, K = 1)}.}
+#'    \item{probY1Z0K1}{Probability \eqn{P(Y = 1 | Z = 0, K = 1)}.}
+#'    \item{p0}{The prior propensity where the probability of a positive causal effect equals the probability of a negative causal effect.}
+#'  }
+#' \item{probassing}{A vector containing probabilies of assignment to treatments Z=1 and Z=0.}Evangelion 3.0+1.0 (2021)
+#' \item{Pi}{Probabilities identified from the data:}
+#'  \itemize{
+#'    \item{EpPi}{Expected value of \eqn{\Pi}.}
+#'    \item{CDF_Pi}{CDF function of \eqn{\Pi}, \eqn{F_{\Pi,p}}.}
+#'    \item{qPileft}{Left-continuous quasi-inverse of \eqn{F_{\Pi,p}}.}
+#'    \item{qPiright}{Right-continuous quasi-inverse of \eqn{F_{\Pi,p}}.}
+#'    \item{q0025left}{Quantile 0.025 of \eqn{\Pi} using the left-continuous quasi-inverse.}
+#'    \item{q0025right}{Quantile 0.025 of \eqn{\Pi} using the right-continuous quasi-inverse.}
+#'    \item{q0975left}{Quantile 0.975 of \eqn{\Pi} using the left-continuous quasi-inverse.}
+#'    \item{q0975right}{Quantile 0.975 of \eqn{\Pi} using the right-continuous quasi-inverse.}
+#'    \item{q0025}{Lower bound of the 95\% credible interval of \eqn{Pi}.}
+#'    \item{q0975}{Upper bound of the 95\% credible interval of \eqn{Pi}.}
+#'  }
+#'
+#' @examples
+#' # Example usage of the CTEprob function
+#' result <- CTEprob(M = 200, nNA = 0, nZ1K1 = 65, nZ0K1 = 135, 
+#'                   nY1Z1K1 = 34, nY1Z0K1 = 91, p = c(0:100)/100 )
+#' result$masses
+#' result$cdf
+#'
+#' @export
+possibleworlds <- function(M, nNAZ1 = 0, nNAZ0 = 0, nNA=NULL, nZ1K1, nZ0K1, 
+                      nY1Z1K1, nY1Z0K1, p, dec.prec=13, alpha=0.05, approxtonorm=FALSE) {
+  
+  if ( (!approxtonorm) & (M>2000) ) {
+    cat(paste0("M is large (M=", M, "). The exact calculation of the Pi distribution may consume a lot of resources. Select whether you want to continue with the exact calculation or use the approximation.\n"))
+    cat("1: Continue with the approximate calculation.\n")
+    cat("2: Continue with the exact calculation.\n")
+    resp <- 0
+    while (!is.element(resp, c(1,2,""))) {
+      #resp <- readline(prompt = "Enter a number (default=1): ")
+      resp <- readline(prompt = "Enter a number (default=1): ")
+    }
+    if ( is.element( resp , c(1,"")) ) {
+      approxtonorm <- TRUE
+    }
+  }
+  
+  if ( is.null(nNA) ) {
+    nNA <- nNAZ1 + nNAZ0
+  } else {
+    nNAZ1 <- nNAZ0 <- nNA/2
+  }
+  
+  n1 <- nZ0K1 + nNA     # M - nZ1K1
+  n2 <- nZ1K1 + nNA
+  
+  a1 <- nY1Z1K1 / M    # P(Y=1|Z=1,K=1) P(K=1|Z=1)
+  b1 <- (M - nZ1K1) / ( M * (nZ0K1 + nNA) )
+  a2 <- nY1Z0K1 / M    # P(Y=1|Z=0,K=1) P(K=1|Z=0)
+  b2 <- (M - nZ0K1) / ( M * (nZ1K1 + nNA) )
+  
+  probK1Z1   <- nZ1K1 / M        # P(K=1|Z=1)
+  probK1Z0   <- nZ0K1 / M        # P(K=1|Z=0)
+  probY1Z1K1 <- nY1Z1K1 / nZ1K1  # P(Y=1|Z=1,K=1)
+  probY1Z0K1 <- nY1Z0K1 / nZ0K1  # P(Y=1|Z=0,K=1)
+  
+  probassingZ1 <- (nNAZ1 + nZ1K1)/M
+  probassingZ0 <- (nNAZ0 + nZ0K1)/M
+  # P(Z=1|K=1) and P(Z=0|K=1)
+  probZ1K1 <- nZ1K1 / (M-nNA)
+  probZ0K1 <- nZ0K1 / (M-nNA)
+  
+  probMZ <- list(probK1Z1 = probK1Z1, probK1Z0 = probK1Z0, 
+                 probY1Z1K1 = probY1Z1K1, probY1Z0K1 = probY1Z0K1,
+                 probZ1K1 = probZ1K1 , probZ0K1 = probZ0K1, 
+                 n1 = n1, n0 = n2, M=M)
+  
+  if (probK1Z1 == 0.5) {
+    probMZ$p0 <- "p0 does not exist"
+  } else {
+    p0 <- (probY1Z1K1 * probK1Z1 - probY1Z0K1 * probK1Z0) / (probK1Z1 - probK1Z0)
+    probMZ$p0 <- p0
+  }
+  
+  
+  if ( approxtonorm ) {
+    
+    # Computing the expected value and variance of \Pi
+    a <- probY1Z1K1*probK1Z1 - probY1Z0K1*probK1Z0
+    EpPi <- a - p*(probK1Z1 - probK1Z0)   # a + p*( (1-probK1Z1) - (1-probK1Z0) )
+    VpPi <- p*(1-p)*(n1+n2) / M^2
+    sqrtVpPi <- sqrt(VpPi)
+    
+    # By compatibility, constructing a dataframe with values of the CDF of PI
+    cdf <- data.frame( cte=seq(-1,1,0.0005) )
+    qalpha2 <- q1malpha2 <- c()
+    
+    # Creating the CDF of Pi for each p and computing quantiles for the CI
+    CDF_Pi <- list()
+    for ( i in 1:length(p) ) {
+      CDF_Pi[[i]] <- function(q) { pnorm(q, EpPi[i], sqrtVpPi[i] ) }
+      qalpha2   <- c( qalpha2  , qnorm( alpha/2, EpPi[i], sqrtVpPi[i] )  )
+      q1malpha2 <- c( q1malpha2, qnorm( 1-alpha/2, EpPi[i], sqrtVpPi[i] )  )
+      cdf <- cbind( cdf , CDF_Pi[[i]](cdf$cte) ) 
+    }
+    
+    names(cdf)[-1] <- paste0("p", p)
+    
+    #masses <- cdf[-1,-1] - cdf[ -nrow(cdf) ,-1]
+    #masses <- rbind( rep(0,length(p)) , masses )
+    #masses <- cbind( cdf$cte , masses )
+    masses <- NULL
+    
+    Pi <- list( EpPi=EpPi , VpPi=VpPi, p=p , CDF_Pi=CDF_Pi,
+                qalpha2=qalpha2 , q1malpha2=q1malpha2 )
+    
+  } else {
+    obj <- CTEprobcpp(n1 = n1, n2 = n2, a1 = a1, b1 = b1, a2 = a2, b2 = b2, p = p)
+    colnames(obj$results) <- obj$namesresults
+    obj$results[,"cte"] <- round( obj$results[,"cte"] , dec.prec)
+    
+    results <- obj$results
+    
+    masses <- data.frame( cte = sort(unique(results[,"cte"])) )
+    cdf <- masses
+    
+    for (i in 1:length(p)) {
+      aux <- aggregate_cpp( values = results[,5+i],
+                            groups = results[,"cte"] ,
+                            levelgroup = masses[,"cte"] )
+      masses <- cbind( masses, aux[,2] )
+      cdf <- cbind( cdf, cumsum(aux[,2]) )
+    }
+    
+    names(masses)[-1] <- paste0("p", p)
+    names(cdf) <- names(masses)
+    
+    # Distribution of \Pi, mean and 95% CI (and alpha CI) for each p
+    
+    CDF_Pi <- list()
+    EpPi <- c()
+    q0025left <- q0025right <- q0975left <- q0975right <- c()
+    qalpha2 <- q1malpha2 <- c()
+    qPileft <- list()
+    qPiright <- list()
+    
+    for ( i in 2:ncol(cdf) ) {
+      CDF_Pi[[i]] <- approxfun(cdf$cte, 
+                               cdf[,i], 
+                               method = "constant", 
+                               yleft = -1, yright = 1, 
+                               f = 0, ties = "ordered")
+      class(CDF_Pi[[i]]) <- c( class(CDF_Pi[[i]]) , "stepfun" )
+      # Expected \Pi
+      EpPi <- c( EpPi , ctepi::Ex(CDF_Pi[[i]])$E )
+      
+      # Assigning values to the stepfun function CDF_Pi[[i]] needed for quasiinverse()
+      assign("vals", cdf$cte, envir = environment(CDF_Pi[[i]]))
+      assign("Fvals", cdf[,i], envir = environment(CDF_Pi[[i]]))
+      
+      # Computing quantiles
+      qileft  <- quasiinverse( list( ecdf = CDF_Pi[[i]] ) ,continuity = "left")
+      qiright <- quasiinverse( list( ecdf = CDF_Pi[[i]] ) ,continuity = "right")
+      # Quantiles of alpha/2 and 1-alpha/2
+      qalpha2 <- c( qalpha2 , qiright( alpha/2 ) )
+      q1malpha2  <- c( q1malpha2 , qileft( 1-alpha/2 ) )
+    }
+    
+    Pi <- list( EpPi=EpPi , p=p , CDF_Pi=CDF_Pi,
+                qalpha2=qalpha2 , q1malpha2=q1malpha2 )
+  }
+  
+  list( masses=masses, cdf=cdf , p=p , probMZ=probMZ,
+        probassing = c(probassingZ1=probassingZ1, probassingZ0=probassingZ0) ,
+        Pi=Pi )
+}
+
+
